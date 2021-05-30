df047b185d20bff0d77ac478710e90d23c725758
Scenaristeur <scenaristeur@gmail.com>
2021-05-30 11:13:27 +0200

    Initial dist subtree commit

---
+++ dist/precache-manifest.9f64a74f51858f2d06cc976325d83a2f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c50a3d2d5540ceb3663f",
+    "url": "/egadnos/css/app.ba014d55.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/egadnos/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "5b6fd70da6a2931f82b177f9c6af423e",
+    "url": "/egadnos/index.html"
+  },
+  {
+    "revision": "a32fab00963dfaed9b49",
+    "url": "/egadnos/js/about.9e6c2f9d.js"
+  },
+  {
+    "revision": "c50a3d2d5540ceb3663f",
+    "url": "/egadnos/js/app.becc8cb8.js"
+  },
+  {
+    "revision": "9a2087fffe5b60501309",
+    "url": "/egadnos/js/chunk-vendors.b7bcc82f.js"
+  },
+  {
+    "revision": "366728f0fbf7bd55f2149baf81ed4ff5",
+    "url": "/egadnos/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/egadnos/robots.txt"
+  }
+]);
